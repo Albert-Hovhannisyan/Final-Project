b0819af7e78238b67071cfda76ce951778d33e50
Albert Hovhannisyan <20albert01@gmail.com>
2022-05-11 18:04:29 +0400

    Material UI

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,25 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import * as React from 'react';
+import ReactDOM from 'react-dom';
+import AppBar from '@mui/material/AppBar';
+import Button from '@mui/material/Button';
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import Box from '@mui/material/Box';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <React.Fragment>
+    <AppBar>
+      <Box m={1} display="flex" justifyContent="center" alignItems="center" fontSize={25}>Welcome</Box>
+      <Box m={1} display="flex" justifyContent="flex-end" alignItems="flex-end">
+        <Button variant="contained">
+          Register
+        </Button>
+        <Button variant="contained">
+          Log In
+        </Button>
+        <Button variant="contained">
+          <ShoppingCartOutlinedIcon fontSize='medium'></ShoppingCartOutlinedIcon>
+        </Button>
+      </Box>
+    </AppBar>
+    </React.Fragment>;
 }
 
 export default App;
